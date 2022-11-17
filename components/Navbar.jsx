81d4a5f0775650a99da09f2bdd8ad052697a1915
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-17 22:51:25 +0100

    feat: Finish with Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,6 +11,7 @@ import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
+import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import CookieIcon from '@mui/icons-material/Cookie';
 
@@ -34,6 +36,35 @@ function Navbar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
+    animation: 'ripple 1.2s infinite ease-in-out',
+    border: '1px solid currentColor',
+    content: '""',
+    },
+    },
+    '@keyframes ripple': {
+    '0%': {
+    transform: 'scale(.8)',
+    opacity: 1,
+    },
+    '100%': {
+    transform: 'scale(2.4)',
+    opacity: 0,
+    },
+    },
+    }));
 
   return (
     <AppBar position="static">
@@ -127,8 +158,14 @@ function Navbar() {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="../src/assets/a.jpg" />
+              <IconButton onClick={ handleOpenUserMenu } sx={ { p: 0 } }>
+                <StyledBadge
+                  overlap="circular"
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                  variant="dot"
+                >
+                  <Avatar alt="Remy Sharp" src="../src/assets/a.jpg" />
+                </StyledBadge>
               </IconButton>
             </Tooltip>
             <Menu
