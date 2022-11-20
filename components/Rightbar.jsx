61f5464eb9551da37ff0c8b9e5257358c4b10e8a
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-11-20 16:25:07 +0100

    feat : Finish with Rightbar component

--- components/Rightbar.jsx
+++ components/Rightbar.jsx
@@ -1,13 +1,17 @@
 import { Box, Typography, Avatar, AvatarGroup, ImageList, ImageListItem } from '@mui/material'
 import React from 'react'
-
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
 
 
 const Rightbar = () => {
   return (
    <Box sx={ { display: { xs: 'none', sm: 'block' } } } flex={ 2 } p={ 2 }>
       <Box position='fixed'>
-        <Box mt={2} sx={{borderButtom:'1px solid black'}}>
+        <Box mt={1} sx={{borderButtom:'1px solid black'}}>
           <Typography fontWeight={ 200 }>Online Friends</Typography>
         </Box>
         <Box mt={1}>
@@ -18,11 +22,11 @@ const Rightbar = () => {
           <Avatar alt="Agnes Walker" src="../src/assets/e.jpg" />
           <Avatar alt="Trevor Henderson" src="../src/assets/f.jpg" />
           </AvatarGroup>
-        <Box mt={2} sx={{borderButtom:'1px solid black'}}>
+        <Box mt={1} sx={{borderButtom:'1px solid black'}}>
           <Typography fontWeight={ 200 }>Latest Pictures</Typography>
         </Box>
         <Box mt={2}>
-          <ImageList sx={{width:200, height:200}} cols={3} rowHeight={100} >
+            <ImageList rowHeight={100} cols={3} gap={5}>
             <ImageListItem>
                 <img
                   src='../src/assets/f.jpg'
@@ -31,7 +35,7 @@ const Rightbar = () => {
             </ImageListItem>
             <ImageListItem>
                 <img
-                  src='../src/assets/e.jpg'
+                  src='../src/assets/g.jpeg'
                   alt=""
                 />  
             </ImageListItem> 
@@ -42,10 +46,81 @@ const Rightbar = () => {
                 />  
             </ImageListItem>  
           </ImageList>
+          <Box mt={2}>
+            <Typography fontWeight={ 200 } >Latest Convesations</Typography>  
+          </Box>  
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {' — Do you have Paris recommendations? Have you ever…'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+   
         </Box>  
-        </Box>
       </Box>
     </Box>
+  </Box>
   )
 }
 
