a9c27210ffdadad65e393fe9425d1668f334fc84
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-03-12 21:11:40 +0100

    fix : resolve useState hook problem

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -12,7 +11,7 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
 const Drawer = ()=> {
-  const [state, setState] = React.useState({
+  const [state, setState] = useState({
     
     left: false,
   });
